f09fbfbb50bec8126a6b0de868c8b3a5a2b58e2c
KEVIN PACHECO <158864387+kpsepulveda@users.noreply.github.com>
2024-02-14 20:17:49 -0500

    Add files via upload

--- C5 Fundamentos Javascript/ejercicios/06.js
+++ C5 Fundamentos Javascript/ejercicios/06.js
@@ -1,6 +1,6 @@
 // Resuelve el siguiente problema matemático.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 4.2;
 
 module.exports = nuevoModulo;
